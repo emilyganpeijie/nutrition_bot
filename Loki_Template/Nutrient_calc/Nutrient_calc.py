59c12dfac3c0923fe693a049eca4e4608dc738ab
emilyganpeijie <ganpeijie3@gmail.com>
2024-08-30 21:31:52 +0800

    update main Loki file

--- Loki_Template/Nutrient_calc/Nutrient_calc.py
+++ Loki_Template/Nutrient_calc/Nutrient_calc.py
@@ -316,15 +316,23 @@ def testIntent():
 
 if __name__ == "__main__":
     # 測試所有意圖
-    testIntent()
+    #testIntent()
 
     # 測試其它句子
     filterLIST = []
     splitLIST = ["！", "，", "。", "？", "!", ",", "\n", "；", "\u3000", ";"]
     # 設定參考資料
-    refDICT = { # value 必須為 list
-        #"key": []
+    refDICT = { 
+        "age": [],
+        "gen": [],
+        "clf": [], 
+        "meas": [],
+        "food": []
     }
-    resultDICT = execLoki("今天天氣如何？後天氣象如何？", filterLIST=filterLIST, refDICT=refDICT)                      # output => {"key": ["今天天氣"]}
-    resultDICT = execLoki("今天天氣如何？後天氣象如何？", filterLIST=filterLIST, splitLIST=splitLIST, refDICT=refDICT) # output => {"key": ["今天天氣", "後天氣象"]}
-    resultDICT = execLoki(["今天天氣如何？", "後天氣象如何？"], filterLIST=filterLIST, refDICT=refDICT)                # output => {"key": ["今天天氣", "後天氣象"]}
+    #resultDICT = execLoki("今天天氣如何？後天氣象如何？", filterLIST=filterLIST, refDICT=refDICT)                      # output => {"key": ["今天天氣"]}
+    #resultDICT = execLoki("今天天氣如何？後天氣象如何？", filterLIST=filterLIST, splitLIST=splitLIST, refDICT=refDICT) # output => {"key": ["今天天氣", "後天氣象"]}
+    #resultDICT = execLoki(["今天天氣如何？", "後天氣象如何？"], filterLIST=filterLIST, refDICT=refDICT)                # output => {"key": ["今天天氣", "後天氣象"]}
+    
+    inputSTR = "我是一位女性"
+    resultDICT = execLoki(inputSTR, refDICT=refDICT)
+    print(resultDICT)    
