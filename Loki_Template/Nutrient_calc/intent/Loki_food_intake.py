f447b0ef94f910846b1959533e5cdd2617cf6752
ellenyp <ivickie621@gmail.com>
2024-08-30 16:20:31 +0800

    Upload Loki_Template

---
+++ Loki_Template/Nutrient_calc/intent/Loki_food_intake.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+"""
+    Loki module for food_intake
+
+    Input:
+        inputSTR      str,
+        utterance     str,
+        args          str[],
+        resultDICT    dict,
+        refDICT       dict,
+        pattern       str
+
+    Output:
+        resultDICT    dict
+"""
+
+from random import sample
+import json
+import os
+
+DEBUG = True
+CHATBOT_MODE = False
+
+userDefinedDICT = {}
+try:
+    userDefinedDICT = json.load(open(os.path.join(os.path.dirname(__file__), "USER_DEFINED.json"), encoding="utf-8"))
+except Exception as e:
+    print("[ERROR] userDefinedDICT => {}".format(str(e)))
+
+responseDICT = {}
+if CHATBOT_MODE:
+    try:
+        responseDICT = json.load(open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "reply/reply_food_intake.json"), encoding="utf-8"))
+    except Exception as e:
+        print("[ERROR] responseDICT => {}".format(str(e)))
+
+# 將符合句型的參數列表印出。這是 debug 或是開發用的。
+def debugInfo(inputSTR, utterance):
+    if DEBUG:
+        print("[food_intake] {} ===> {}".format(inputSTR, utterance))
+
+def getResponse(utterance, args):
+    resultSTR = ""
+    if utterance in responseDICT:
+        if len(responseDICT[utterance]):
+            resultSTR = sample(responseDICT[utterance], 1)[0].format(*args)
+
+    return resultSTR
+
+def getResult(inputSTR, utterance, args, resultDICT, refDICT, pattern=""):
+    debugInfo(inputSTR, utterance)
+    if utterance == "100g世界上最好吃的雞胸":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+            if resultDICT["response"]:
+                resultDICT["source"] = "reply"
+        else:
+            # write your code here
+            resultDICT[meas].append(0)
+            resultDICT[food].append(2)
+            pass
+
+    if utterance == "一杯超級好喝的奶茶":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+            if resultDICT["response"]:
+                resultDICT["source"] = "reply"
+        else:
+            # write your code here
+            # resultDICT[key].append(value)
+            pass
+
+    if utterance == "一碗100克的白飯":
+        if CHATBOT_MODE:
+            resultDICT["response"] = getResponse(utterance, args)
+            if resultDICT["response"]:
+                resultDICT["source"] = "reply"
+        else:
+            # write your code here
+            # resultDICT[key].append(value)
+            pass
+
+    return resultDICT
