8cb4ceb692c2c69eb2128c2a7003643d0d09b871
ellenyp <ivickie621@gmail.com>
2024-09-01 02:54:53 +0800

    Add new utterance to "Loki_food_intake"

--- Loki_Template/Nutrient_calc/intent/Loki_food_intake.py
+++ Loki_Template/Nutrient_calc/intent/Loki_food_intake.py
@@ -60,6 +60,15 @@ def getResult(inputSTR, utterance, args, resultDICT, refDICT, pattern=""):
             resultDICT["clf"].append(args[1])
             resultDICT["meas"].append(args[3])
             resultDICT["food"].append(args[8])
-            pass
+    
+            if utterance == "我吃了一份雞胸":
+                if CHATBOT_MODE:
+                    resultDICT["response"] = getResponse(utterance, args)
+                    if resultDICT["response"]:
+                        resultDICT["source"] = "reply"
+                else:
+                    resultDICT["clf"].append(args[7])
+                    resultDICT["meas"].append(args[9])
+                    resultDICT["food"].append(args[13])    
 
     return resultDICT
