73d24d893f09a2aca808eaf3d6e6b4a158310ca9
ellenyp <ivickie621@gmail.com>
2024-09-05 17:34:28 +0800

    change updatetime

--- Loki_Template/Nutrient_calc/Discord_bot_nutrition_bot.py
+++ Loki_Template/Nutrient_calc/Discord_bot_nutrition_bot.py
@@ -85,14 +85,14 @@ class BotClient(discord.Client):
             elif msgSTR.lower() in ["哈囉","嗨","你好","您好","hi","hello"]:
                 #有講過話(判斷對話時間差)
                 if message.author.id in self.mscDICT.keys():
-                    timeReset = datetime.time(3,0,0,0, tzinfo=datetime.timezone(datetime.timedelta(hours=8)))
-                    #設定更新紀錄時間為台灣時區每天凌晨三點
+                    #timeReset = datetime.time(3,0,0,0, tzinfo=datetime.timezone(datetime.timedelta(hours=8)))
+                    ##設定更新紀錄時間為台灣時區每天凌晨三點
                     timeDIFF = datetime.now() - self.mscDICT[message.author.id]["updatetime"]
                     #有講過話，但與上次差超過 5 分鐘(視為沒有講過話，刷新template)
-                    if timeDIFF.total_seconds() >= 300:
+                    if timeDIFF.total_secinds() >= 1800:
                         self.mscDICT[message.author.id] = self.resetMSCwith(message.author.id)
                         replySTR = "嗨嗨，我們好像見過面，但卓騰的隱私政策不允許我記得你的資料，抱歉！"
-                    #有講過話，而且還沒超過5分鐘就又跟我 hello (就繼續上次的對話)
+                    #有講過話，而且還沒超過30分鐘就又跟我 hello (就繼續上次的對話)
                     else:
                         replySTR = self.mscDICT[message.author.id]["latestQuest"]
                 #沒有講過話(給他一個新的template)
